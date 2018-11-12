73b06cce28a71d870084c063d74201fd371a590b
Yoel Monzon <yoelfme@hotmail.com>
2018-11-12 12:43:13 -0600

    [update] tests for main file with new es6 featuers

--- test/index.js
+++ test/index.js
@@ -1,14 +1,14 @@
-'use strict'
-
 const Catbox = require('catbox')
 const Code = require('code')
 const Lab = require('lab')
 const Cache = require('../lib/index')
 
 // Tests shorcuts
-const lab = exports.lab = Lab.script()
+const lab = (exports.lab = Lab.script()); // eslint-disable-line
 const { expect } = Code
-const { describe, it, before, afterEach } = lab
+const {
+  describe, it, before, afterEach,
+} = lab
 
 const internals = {
   defaultOptions: {
@@ -18,193 +18,193 @@ const internals = {
       host: '127.0.0.1',
       port: 6379,
       password: '',
-      database: 'cache'
-    }
+      database: 'cache',
+    },
   },
   testKey: {
     key: 'ABC',
     invalidKey: 'ABD',
     segment: 'tests',
-    value: 123
-  }
+    value: 123,
+  },
 }
 
 describe('Cache package', () => {
   describe('initializing server', () => {
-    afterEach((done) => {
+    afterEach(() => {
       Cache.stop()
-      done()
     })
 
-    it('should able to start client well', (done) => {
-      Cache.start().then(() => {
-        expect(Cache.set).to.be.exists()
-        expect(Cache.get).to.be.exists()
-        expect(Cache.start).to.be.exists()
-        expect(Cache.isReady()).to.equals(true)
-        done()
-      })
+    it('should able to start client well', async () => {
+      await Cache.start()
+
+      expect(Cache.set).to.be.exists()
+      expect(Cache.get).to.be.exists()
+      expect(Cache.start).to.be.exists()
+      expect(Cache.isReady()).to.equals(true)
     })
 
-    it('should able to manage error when trying to init the client', done => {
-      const start = Catbox.Client.prototype.start
-      Catbox.Client.prototype.start = (callback) => {
-        callback(new Error('Cannot start client well'))
+    it('should able to manage error when trying to init the client', async () => {
+      const { start } = Catbox.Client.prototype
+      Catbox.Client.prototype.start = async () => {
+        throw new Error('Cannot start client well')
       }
 
-      Cache.start().catch((error) => {
-        expect(error).to.be.instanceof(Error)
-        expect(error.message).to.equals('Cannot start client well')
+      try {
+        await Cache.start()
+      } catch (err) {
+        expect(err).to.be.instanceof(Error)
+        expect(err.message).to.equals('Cannot start client well')
         Catbox.Client.prototype.start = start
-        done()
-      })
-    })
-
-    it('should able to validate connection to cache server', (done) => {
-      Cache.validateConnection()
-        .catch((e) => {
-          expect(e).to.be.an.error()
-          expect(e.message).to.equals('Start a new connection is necessary')
-          done()
-        })
-    })
-
-    it('should able to throw an error if client is instanced but not ready', (done) => {
-      const isReady = Catbox.Client.prototype.isReady
-      Catbox.Client.prototype.isReady = () => {
-        return false
       }
-
-      Cache.start()
-        .then(() => Cache.validateConnection())
-        .catch((error) => {
-          expect(error).to.be.an.error()
-          expect(error.message).to.equals('Client is not ready')
-          Catbox.Client.prototype.isReady = isReady
-          done()
-        })
     })
 
-    it('show able to throw an error if client is trying to start again', (done) => {
-      Cache.start()
-        .then(() => Cache.start())
-        .catch((error) => {
-          expect(error).to.be.an.error()
-          expect(error.message).to.equals('Client has been initialized')
-          done()
-        })
+    it('should able to validate connection to cache server', () => {
+      try {
+        Cache.validateConnection()
+      } catch (err) {
+        expect(err).to.be.an.error()
+        expect(err.message).to.equals('Start a new connection is necessary')
+      }
     })
 
-    it('show able to verify if client is ready', (done) => {
-      Cache.start().then(() => {
-        const isReady = Cache.isReady()
+    it('should able to throw an error if client is instanced but not ready', async () => {
+      const { isReady } = Catbox.Client.prototype
+      Catbox.Client.prototype.isReady = () => false
 
-        expect(isReady).to.be.a.boolean()
-        expect(isReady).to.equals(true)
-        done()
-      })
+      try {
+        await Cache.start()
+        Cache.validateConnection()
+      } catch (err) {
+        expect(err).to.be.an.error()
+        expect(err.message).to.equals('Client is not ready')
+        Catbox.Client.prototype.isReady = isReady
+      }
     })
 
-    it('show able to verify if client is not ready', (done) => {
+    it('show able to throw an error if client is trying to start again', async () => {
+      try {
+        await Cache.start()
+        await Cache.start()
+      } catch (err) {
+        expect(err).to.be.an.error()
+        expect(err.message).to.equals('Client has been initialized')
+      }
+    })
+
+    it('show able to verify if client is ready', async () => {
+      await Cache.start()
+
+      const isReady = Cache.isReady()
+
+      expect(isReady).to.be.a.boolean()
+      expect(isReady).to.equals(true)
+    })
+
+    it('show able to verify if client is not ready', () => {
       const isReady = Cache.isReady()
 
       expect(isReady).to.be.a.boolean()
       expect(isReady).to.equals(false)
-      done()
     })
   })
 
   describe('call functions', () => {
-    before((done) => {
-      Cache.start({ debug: true }).then(() => {
-        done()
-      })
+    before(async () => {
+      await Cache.start({ debug: true })
     })
 
-    it('should able to store a new value', done => {
-      Cache.set(internals.testKey.segment, internals.testKey.key, internals.testKey.value)
-        .then(success => {
-          expect(success).to.be.true()
-          done()
-        })
+    it('should able to store a new value', async () => {
+      const success = await Cache.set(
+        internals.testKey.segment,
+        internals.testKey.key,
+        internals.testKey.value,
+      )
+      expect(success).to.be.true()
     })
 
-    it('should able to throw a new error when trying to set a key', done => {
-      const set = Catbox.Client.prototype.set
-      Catbox.Client.prototype.set = (key, value, ttl, callback) => {
-        return callback(new Error('fail'))
+    it('should able to throw a new error when trying to set a key', async () => {
+      const { set } = Catbox.Client.prototype
+      Catbox.Client.prototype.set = async () => {
+        throw new Error('fail')
       }
 
-      Cache.set(internals.testKey.segment, internals.testKey.key, internals.testKey.value)
-        .catch(err => {
-          expect(err).to.be.instanceof(Error)
-          Catbox.Client.prototype.set = set
-          done()
-        })
+      try {
+        await Cache.set(
+          internals.testKey.segment,
+          internals.testKey.key,
+          internals.testKey.value,
+        )
+      } catch (err) {
+        expect(err).to.be.instanceof(Error)
+        Catbox.Client.prototype.set = set
+      }
     })
 
-    it('should able to store and get a key', done => {
-      Cache.set(internals.testKey.segment, internals.testKey.key, internals.testKey.value)
-        .then(success => {
-          expect(success).to.be.true()
+    it('should able to store and get a key', async () => {
+      const success = await Cache.set(
+        internals.testKey.segment,
+        internals.testKey.key,
+        internals.testKey.value,
+      )
+      expect(success).to.be.true()
 
-          return Cache.get(internals.testKey.segment, internals.testKey.key)
-        })
-        .then(value => {
-          expect(value).to.equals(123)
-          done()
-        })
+      const value = await Cache.get(
+        internals.testKey.segment,
+        internals.testKey.key,
+      )
+      expect(value).to.equals(123)
     })
 
-    it('should able to manage error when trying to get an invalid key', done => {
-      Cache.get(internals.testKey.segment, internals.testKey.invalidKey)
-        .catch(value => {
-          expect(value).to.be.null()
-          done()
-        })
+    it('should able to manage error when trying to get an invalid key', async () => {
+      const value = await Cache.get(
+        internals.testKey.segment,
+        internals.testKey.invalidKey,
+      )
+      expect(value).to.be.null()
     })
 
-    it('should able to manage if an exception occurred when trying to get a key', done => {
-      const get = Catbox.Client.prototype.get
-      Catbox.Client.prototype.get = (key, callback) => {
-        return callback(new Error('fail'), null)
+    it('should able to manage if an exception occurred when trying to get a key', async () => {
+      const { get } = Catbox.Client.prototype
+      Catbox.Client.prototype.get = async () => {
+        throw new Error('fail')
       }
 
-      Cache.get(internals.testKey.segment, internals.testKey.key)
-        .catch(error => {
-          expect(error).to.be.instanceof(Error)
-          Catbox.Client.prototype.get = get
-          done()
-        })
+      try {
+        await Cache.get(internals.testKey.segment, internals.testKey.key)
+      } catch (err) {
+        expect(err).to.be.instanceof(Error)
+        Catbox.Client.prototype.get = get
+      }
     })
 
-    it('should able to delete a key', done => {
-      Cache.set(internals.testKey.segment, internals.testKey.key, internals.testKey.value)
-        .then(success => {
-          expect(success).to.be.true()
+    it('should able to delete a key', async () => {
+      const saved = await Cache.set(
+        internals.testKey.segment,
+        internals.testKey.key,
+        internals.testKey.value,
+      )
+      expect(saved).to.be.true()
 
-          return Cache.delete(internals.testKey.segment, internals.testKey.key)
-        })
-        .then(success => {
-          expect(success).to.be.true()
-
-          done()
-        })
+      const deleted = await Cache.delete(
+        internals.testKey.segment,
+        internals.testKey.key,
+      )
+      expect(deleted).to.be.true()
     })
 
-    it('should able to manage if an exception ocurred when trying to delete a key', done => {
-      const drop = Catbox.Client.prototype.drop
-      Catbox.Client.prototype.drop = (key, callback) => {
-        return callback(new Error('fail'), null)
+    it('focus should able to manage if an exception ocurred when trying to delete a key', async () => {
+      const { drop } = Catbox.Client.prototype
+      Catbox.Client.prototype.drop = async () => {
+        throw new Error('fail')
       }
 
-      Cache.delete(internals.testKey.segment, internals.testKey.key)
-        .catch(error => {
-          expect(error).to.be.instanceof(Error)
-          Catbox.Client.prototype.drop = drop
-          done()
-        })
+      try {
+        await Cache.delete(internals.testKey.segment, internals.testKey.key)
+      } catch (err) {
+        expect(err).to.be.instanceof(Error)
+        Catbox.Client.prototype.drop = drop
+      }
     })
   })
 })
-
